7ae25326b05bd2de532082369de1bef28cc80d53
Mark John Humilde <markjohnhumilde14@gmail.com>
2026-08-31 12:10:01 +0800

    feat(web): add design tokens, typography, and global styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource-variable/familjen-grotesk/wght.css";
+import "@fontsource-variable/atkinson-hyperlegible-next/wght.css";
+import "@fontsource-variable/atkinson-hyperlegible-next/wght-italic.css";
 import "./styles/index.css";
 
 import { RouterProvider } from "react-router/dom";
